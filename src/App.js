d55b4bf73b531b1fe54cc551d2e3de62a727b6a3
jyj <yunsky-@naver.com>
2023-05-03 09:01:24 +0900

    크롬 확장프로그램 React Developer Tools 설치 및 성능개선을 위한 lazy,suspense 셋팅

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { Suspense, lazy, useEffect, useState } from 'react';
 import bg from './img/bg.png'
 import { Button, Container, Nav, Navbar, Spinner } from 'react-bootstrap';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
-import Detail from './Component/Detail';
+// import Detail from './Component/Detail';
+// import Product from './Component/Product';
 import ProductList from './Component/ProductList';
-import Product from './Component/Product';
 import Cart from './Component/Cart';
 import Event from './Component/Event';
 import data from './data'
 import axios from 'axios';
 import WatchProduct from './Component/WatchProduct';
+
+const Detail = lazy(()=> import('./Component/Detail'))
+const Product = lazy(()=> import('./Component/Product'))
+//메인페이지에서 먼저 로드할 필요가 없기 때문에 lazy를 이용한다.
+//lazy는 내가 필요할때 import해주세요라는 말이다.(초기 로딩시간을 줄여줌) -성능 개선
+//단점은 해당 페이지로 들어갈때 load시간이 발생한다.
 
 function App() {
   let [shoes,changeShoes] = useState(data)
@@ -65,8 +71,8 @@ function App() {
         </Container>
       </Navbar>
     
-
-
+    {/* suspense를 이용해 lazy시간동안 보여줄 내용을 적어준다 */}
+    <Suspense fallback={<div>로딩중입니다.</div>}>
       <Routes>   
         <Route path="/" element={ 
           <>
@@ -107,7 +113,7 @@ function App() {
         </Route>
         <Route path="*" element={ <div>없는 페이지입니다.</div> } />
       </Routes>
-     
+    </Suspense>
     </div>
   );
 }
